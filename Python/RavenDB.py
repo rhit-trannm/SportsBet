7d27b578c75ca07c86485965b69fbf1115a11ad6
rhit-trannm <trannm@rose-hulman.edu>
2022-10-10 16:38:53 -0400

    1

--- Python/RavenDB.py
+++ Python/RavenDB.py
@@ -1,5 +1,4 @@
 from pyravendb.store import document_store
-
 class Foo(object):
    def __init__(self, name, key = None):
        self.name = name
@@ -11,11 +10,11 @@ class FooBar(object):
         self.foo = foo
 
 if __name__ == '__main__':
-    with document_store.DocumentStore(urls=["http://137.112.104.162:8080/"], database="temp") as store:
+    with document_store.DocumentStore(urls=["http://137.112.104.162:8080"], database="temp") as store:
         store.initialize()
         print("hello 2")
         with store.open_session() as session:
             print("hello world")
             foo = Foo("PyRavenDB")
-            faa = session.store(foo)
+            session.store(foo)
             session.save_changes()
