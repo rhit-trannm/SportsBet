5831b3241ae101856815581828ca1e65a35e936a
rhit-mestemjw <83134926+rhit-mestemjw@users.noreply.github.com>
2022-10-10 16:32:25 -0400

    Added checking for username to be unique and modified user creation somewhat

--- Python/Redis.py
+++ Python/Redis.py
@@ -6,14 +6,20 @@ def ConnectRedis():
             host='433-17.csse.rose-hulman.edu',
             port=6379,
             decode_responses=True)
-def CreateUser(username, password):
+def CreateUser(username, password, birthday):
     #User key format: User_{Username}
     passwordSalt = bcrypt.gensalt()
 
     hashPassword = bcrypt.hashpw(password, passwordSalt)
-    r.sadd(f'UsernameSalt:{username}', passwordSalt)
+    r.sadd(f'UsernameSalt:{username}', passwordSalt) #this and below line may not need anymore, didn't remove bc didn't know what they are for -Josh
     r.sadd(f'UsernamePassword:{username}', hashPassword)
-    r.hset(f'User:{username}', 'balance', 'nil', 'Bets', 'nil')
+
+    if r.sismember(f'users', username):
+        raise Exception('Username not unique')
+    r.sadd(f'users', username)
+    r.hset(username, 'passwordHash', hashPassword)
+    r.hset(username, 'birthday', birthday)
+    r.hset(f'User:{username}', 'balance', 'nil', 'Bets', 'nil') #don't know if need this as we didn't agree for bets and balances to be put in Redis
 def ChangeUserInformation(field, value):
     print('x')
 
