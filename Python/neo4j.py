ff53eddb38cd6fbda38683236ea397004bc8a82c
rhit-mestemjw <83134926+rhit-mestemjw@users.noreply.github.com>
2022-10-28 02:49:56 -0400

    Added creating moneyline bets and over\under bets for neo4j

--- Python/neo4j.py
+++ Python/neo4j.py
@@ -2,6 +2,7 @@ from matplotlib.backend_bases import GraphicsContextBase
 from py2neo import Graph
 import bcrypt
 from regex import F
+from pyravendb.store import document_store
 
 
 
@@ -35,4 +36,23 @@ def Login_Check(username, password):
     correctPassword = graph.run(f"MATCH(User) WHERE User.username = {username} RETURN User.passwordHash")
     if(password == correctPassword):
         return True
+
+def Create_MoneyLine_Bet(game_date, winner_team_abbrev, amount_betted, username): #I assume team abbrev is a drop down for this,  and so is game
+    ConnectNeo4J
+    userExists = graph.run(f"MATCH (u:User) WHERE User.username = {username} WITH COUNT(u) > {0} as node_exists RETURN node_exists")
+    if(userExists):
+        if(amount_betted > 0):
+            #actually create bet, will implement balance checking here sometime soon,
+            graph.run(f"CREATE (n:MoneyLineBet {{user: '{username}', amountBetted: '{amount_betted}', winnerAbbrev: '{winner_team_abbrev}', game_date'{game_date}'}})")
+            graph.run(f"MATCH (u:User) WHERE u.username = {username} SET u.balance = u.balance - {amount_betted}") #adjust user balance,
+
+def Create_OverUnder_Bet(game_date, stat_type_abbrev, amount_betted, username, isUnder, stat_bet): #isUnder is 1 or 0 (meaning its an over bet), I assume isUnder will be a drop down
+    #box, same for stat_type_abbrev, I assume amount_betted will be an integer. stat_bet is amount you are betting a stat will be under or over, technically in real betting not controlled by you
+    userExists = graph.run(f"MATCH (u:User) WHERE User.username = {username} WITH COUNT(u) > {0} as node_exists RETURN node_exists")
+    if(userExists):
+        if(amount_betted > 0):
+            #actually create bet, will implement balance checking here sometime soon,
+            graph.run(f"CREATE (n:OverUnderBet {{user: '{username}', amountBetted: '{amount_betted}', isUnder: '{isUnder}', game_date: '{game_date}', stat_type: '{stat_type_abbrev}', stat_bet: '{stat_bet}'}})")
+            graph.run(f"MATCH (u:User) WHERE u.username = {username} SET u.balance = u.balance - {amount_betted}") #adjust user balance,
+
 
