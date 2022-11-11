d5754a8f495541856e34b122969ebf25b09c337f
rhit-mestemjw <83134926+rhit-mestemjw@users.noreply.github.com>
2022-11-10 22:09:02 -0500

    added updating/removing friends

--- Python/neo4j.py
+++ Python/neo4j.py
@@ -55,3 +55,43 @@ if __name__ == '__main__':
     ConnectNeo4J()
     #Create_User("billy", "billy2", "billa", "10-2-22")
     GetUser("billy2")
+
+def Create_MoneyLine_Bet(game_date, winner_team_abbrev, amount_betted, username, matchId): #I assume team abbrev is a drop down for this,  and so is matchId and game_date
+    ConnectNeo4J
+    userExists = graph.run(f"MATCH (u:User) WHERE User.username = {username} WITH COUNT(u) > {0} as node_exists RETURN node_exists")
+    if(userExists):
+        if(amount_betted > 0):
+            #actually create bet, will implement balance checking here sometime soon,
+            graph.run(f"CREATE (n:MoneyLineBet {{user: '{username}', amountBetted: '{amount_betted}', winnerAbbrev: '{winner_team_abbrev}', game_date: '{game_date}', gameID: '{matchId}'}})")
+            graph.run(f"MATCH (u:User) WHERE u.username = {username} SET u.balance = u.balance - {amount_betted}") #adjust user balance,
+
+def Create_OverUnder_Bet_Team(game_date, amount_betted, username, isUnder, points_bet, matchId, teamId): #isUnder is 1 or 0 (meaning its an over bet), I assume isUnder will be a drop down
+    #box, I assume amount_betted will be an integer. points_bet is amount you are betting point count for a team will be under or over, technically in real betting not controlled by you
+    #I assume gameId and game_date and teamId is point and click or drop done or automatically determined by ravenDb and sent here as inputs.
+    userExists = graph.run(f"MATCH (u:User) WHERE User.username = {username} WITH COUNT(u) > {0} as node_exists RETURN node_exists")
+    if(userExists):
+        if(amount_betted > 0):
+            #actually create bet, will implement balance checking here sometime soon,
+            graph.run(f"CREATE (n:OverUnderBetPlayer {{user: '{username}', amountBetted: '{amount_betted}', teamId: '{teamId}', isUnder: '{isUnder}', game_date: '{game_date}', points_bet: '{points_bet}', matchId: '{matchId}'}})")
+            graph.run(f"MATCH (u:User) WHERE u.username = {username} SET u.balance = u.balance - {amount_betted}") #adjust user balance,
+
+
+def Payout_MoneyLine_Bets(currentDate, winningTeamAbbrev): #we assume a team can't play 2 games in one day for this method, going with double payout for now for simplicity's sake - Josh Mestemacher
+    graph.run(f"MATCH (b.MoneyLineBet) WHERE b.data < {currentDate} AND b.winnerAbbrev = {winningTeamAbbrev} WITH C MATCH (u.User) WHERE C.user = u.username SET u.balance = u.balance + 2 * b.amountBetted") #reward winners
+    graph.run(f"MATCH (b.MoneyLineBet) WHERE b.data < {currentDate} DETACH DELETE b") #delete old moneyline bets (including both winning and losing ones)
+
+#Rewarding over\under bets will be difficult until I fully understand RavenDB -Josh Mestemacher
+
+def Add_Friend(userUsername, userFriendUsername): #note that I assume both usernames are 
+    #actual usernames in the system here, I may go back and change this later.
+    graph.run(f"MATCH (u1:Person), (u2:Person) WHERE u1.username = '{userUsername}' AND u2.username = '{userFriendUsername}' CREATE (u1)-[r:friend_of]->(u2)")
+
+def Remove_Friend(userUsername, userFriendUsername):
+    #note that I assume both usernames are 
+    #actual usernames in the system here and that the inputted friend 
+    #actually is a friend of the user for simplicity's sake for the demo, I may go
+    #back and change this later
+    graph.run(f"MATCH (u.User) WHERE u.username = {userUsername} AND EXISTS(u.friends) SET n.friends = [x IN n.friends WHERE x <> '{userFriendUsername}']")
+
+
+
